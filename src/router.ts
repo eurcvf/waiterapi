0ce4d15d1397a7912ebb3872c9429f19bb21a940
Roberto Caro Varella Filho <ti04@gestoracontabilidade.local>
2023-12-08 11:31:36 -0300

    Configurado rotas

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,49 @@
+import { Router } from 'express'
+
+const router = Router()
+
 // list categories
+router.get('/categories', (req,res) => {
+  res.send('OK')
+})
 
 // create category
+router.post('/categories', (req,res) => {
+  res.send('OK')
+})
 
 // list products
+router.get('/products', (req,res) => {
+  res.send('OK')
+})
 
 // create product
+router.post('/products', (req,res) => {
+  res.send('OK')
+})
 
 // get products by category
+router.get('/categories/:categoryId/products', (req,res) => {
+  res.send('OK')
+})
 
 // list orders
+router.get('/orders', (req,res) => {
+  res.send('OK')
+})
 
 // create order
+router.post('/orders', (req,res) => {
+  res.send('OK')
+})
 
 // change order status
+router.put('/orders', (req,res) => {
+  res.send('OK')
+})
 
 // delete/cancel order
+router.delete('/orders', (req,res) => {
+  res.send('OK')
+})
 
